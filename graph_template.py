b780cdd1e810a64ae473fcd8e9df64a48b89b317
tomPlus353 <80232975+tomPlus353@users.noreply.github.com>
2021-03-12 22:39:26 +0900

    Update graph_template.py

--- graph_template.py
+++ graph_template.py
@@ -15,41 +15,50 @@ import random
 
 class Vertex(object):
     def __init__(self, value):
+        self.value = value
+        self.adjacent = {} # keep track of nodes that have an edge from this vertex
+        self.neighbors = []
+        self.neighbors_frequency = []
         pass
 
-    def add_edge_to(self, vertex, weight=0):
-        pass
+    def add_edge_to(self, vertex, frequency=0):
+        self.adjacent[vertex] = frequency
 
     def increment_edge(self, vertex):
-        pass
+        self.adjacent[vertex] = self.adjacent.get(vertex, 0) +  1
 
     def get_adjacent_nodes(self):
-        pass
+        self.adjacent
 
     # initializes probability map
     def get_probability_map(self):
-        pass
+        for (vertex, frequency) in self.adjacent.items():
+            self.neighbors.append(vertex)
+            self.neighbors_frequency.append(frequency)
 
     def next_word(self):
-        pass
+        return random.choices(self.neighbors, weights=self.neighbors_frequency)[0] 
 
 
 
 class Graph(object):
     def __init__(self):
-        pass
+        self.vertices = {} # why not list? - because key = the word value is the vertex of the word
 
     def get_vertex_values(self):
-        pass
+        return set(self.vertices.keys())
 
     def add_vertex(self, value):
-        pass
+        self.vertices[value] = Vertex(value) # powerful: creates vertex and a place for it within the graph
 
     def get_vertex(self, value):
-        pass
+        if value not in self.vertices:
+            self.add_vertex(value)
+        return self.vertices[value]
 
     def get_next_word(self, current_vertex):
-        pass
+        return self.vertices[current_vertex.value].next_word()
 
     def generate_probability_mappings(self):
-        pass
+        for vertex in self.vertices.values():
+            vertex.get_probability_map()
